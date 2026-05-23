a40ea5763ea20c2addba6080af4c5c0fda6dbe97
Joko-26 <playerone99@outlook.de>
2026-05-23 21:09:14 +0200

    :sparkles: finished visual display of status effects

---
+++ effects.md
@@ -0,0 +1,12 @@
+0: Poison (blindness, slowness)
+1: Shock (dazzle)
+2: Frost (slowness, increased damage taken)
+3: Death (ever increasing damage that stops at 1hp)
+4: Fire (continuous damage)
+5: Regeneration (healing)
+6: Sleep (slowness, can’t attack)
+7: Confusion (reversed controlls)
+8: Slowness
+9: Speed
+10: Weakness
+11: Strenght
--- scripts/components/effect_display.gd
+++ scripts/components/effect_display.gd
@@ -10,6 +10,11 @@ func update():
 
 
 func _ready() -> void:
-	$Sprite2D.texture = load(effect.sprite)
 	$ProgressBar.max_value = effect.capacity
 	$ProgressBar.value = effect.amount
+	if effect.active:
+		$ProgressBar.texture_under = load(effect.assets[effect.id]["active_empty"])
+		$ProgressBar.texture_progress = load(effect.assets[effect.id]["active_full"])
+	else:
+		$ProgressBar.texture_under = load(effect.assets[effect.id]["empty"])
+		$ProgressBar.texture_progress = load(effect.assets[effect.id]["full"])
