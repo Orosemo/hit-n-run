43f419d87d52db4d86af227fe3e3a1f34cbef2d0
Joko-26 <playerone99@outlook.de>
2026-06-03 20:59:10 +0200

    :construction: worked on animation controller (again)

--- scripts/components/anim_controller.gd
+++ scripts/components/anim_controller.gd
@@ -8,15 +8,14 @@ var anim_nodes: Dictionary[String, Node2D]
 var priority : Dictionary[String, bool]
 
 func play_all(anim_name: String, is_priority: bool = false):
-	print(anim_name)
 	for anim_node in anim_nodes:
-		if not anim_nodes[anim_node].animation == anim_name and not priority:
+		if anim_nodes[anim_node].animation != anim_name and not priority[anim_node]:
 			anim_nodes[anim_node].play(anim_name)
 			priority[anim_node] = is_priority
 
 func play_for_one(anim_name: String, anim_node: String, is_priority: bool = false):
 	if anim_nodes.has(anim_node):
-		if not anim_nodes[anim_node].animation == anim_name and not priority:
+		if not anim_nodes[anim_node].animation == anim_name and not priority[anim_node]:
 			anim_nodes[anim_node].play(anim_name)
 			priority[anim_node] = is_priority
 	else:
@@ -25,7 +24,7 @@ func play_for_one(anim_name: String, anim_node: String, is_priority: bool = fals
 func play_for_multiple(anim_name: String, anim_node_names: Array[String], is_priority: bool = false):
 	for anim_node_name in anim_node_names:
 		if anim_nodes.has(anim_node_name):
-			if not anim_nodes[anim_node_name].animation == anim_name and not priority:
+			if not anim_nodes[anim_node_name].animation == anim_name and not priority[anim_node_name]:
 				anim_nodes[anim_node_name].play(anim_name)
 				priority[anim_node_name] = is_priority
 		else:
@@ -37,7 +36,6 @@ func set_direction_for_all(direction: Vector2):
 
 func set_direction_for_one(direction: Vector2, anim_node: String):
 	anim_nodes[anim_node].scale = direction
-	push_error("No anim node named " + anim_node)
 
 func set_direction_for_multiple(direction: Vector2, anim_node_names: Array[String]):
 	for anim_node_name in anim_node_names:
@@ -46,15 +44,15 @@ func set_direction_for_multiple(direction: Vector2, anim_node_names: Array[Strin
 		else:
 			push_error("No anim node named " + anim_node_name)
 
-func unset_priority(node_name: String):
-	priority[node_name] = false
+func unset_priority():
+	for anim_node in priority:
+		priority[anim_node] = false
 
 func _ready():
 	for anim_node in anim_nodes_array:
 		priority[anim_node.own_name] = false
 		anim_nodes[anim_node.own_name] = anim_node
-		var own_name = anim_node.own_name
-		anim_node.animation_finished.connect(Callable(self, "unset_priority").bind(own_name))
+		anim_node.animation_finished.connect(unset_priority)
 
 
 	
